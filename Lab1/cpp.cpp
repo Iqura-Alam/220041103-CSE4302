e768dfecb3086f7c172a1b4aa982b1e04a2fc429
Iqura Alam Roza <158697968+Iqura-Alam@users.noreply.github.com>
2024-09-07 19:38:05 +0600

    Testing if it works

---
+++ Lab1/cpp.cpp
@@ -0,0 +1,41 @@
+#include<bits/stdc++.h>
+using namespace std;
+//void printvector(vector<int> &v){
+//        for(int i=0;i<v.size();i++){
+//            cout<<v[i]<<" ";
+//        }
+//    }
+int main(){
+////    //pair
+////    pair<int,string> p;
+////    p=make_pair(2,"abc");
+////    p={2,"abc"};
+////    //pair<int,string> p1=p;
+////    pair<int,int> p_array[3];
+////    p_array[0]={1,2};
+////    p_array[1]={2,3};
+////    p_array[2]={3,4};
+////    swap(p_array[0], p_array[2]);
+////    cout<<p.first<<" "<<p.second<<endl;
+////    for(int i=0;i<3;i++){
+////        cout<<p_array[i].first<<" "<<p_array[i].second<<endl;
+////    }
+//
+//
+//    //vector
+//    //vector<int> v(5,3); 3 3 3 3 3
+//    vector<int> v; //dynamic array
+//    int n;
+//    cin>>n;
+//    for(int i=0;i<n;i++){
+//        int x;
+//        cin>>x;
+//        v.push_back(x);//pushback inserts the number at the end
+//        //v.pop_back(x); exerts number from end;
+//    }
+//    //vector<int> v2=v;
+//    printvector(v);
+
+
+
+}
